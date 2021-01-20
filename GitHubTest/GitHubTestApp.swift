9e13e8ee85dbb1e77e929e298e50091d40c7398b
Jack <hoverbug@btinternet.com>
2021-01-20 18:32:43 +0000

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Finnis on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
